7f7cb08bfd57b5639df3e4e681fc1830c5b64497
igsnow <zzy716810918@gmail.com>
2019-04-04 14:11:48 +0800

    虚拟DOM手写

---
+++ vDOM/virtualDOM.js
@@ -0,0 +1,52 @@
+// DOM树构造函数
+function Element({tagName, props, children}) {
+    if (!(this instanceof Element)) {
+        return new Element({tagName, props, children})
+    }
+    this.tagName = tagName
+    this.props = props || {}
+    this.children = children || []
+}
+
+Element.prototype.render = function () {
+    let el = document.createElement(this.tagName)
+    for (let key in this.props) {
+        el.setAttribute(key, this.props[key])
+    }
+    this.children.forEach(item => {
+        let childEl = null
+        if (item instanceof Element) {
+            childEl = item.render()
+        } else {
+            // 当Children数组子项不再是构造函数Element的实例的时候，只是个单纯的字符串，则创建文本节点
+            childEl = document.createTextNode(item)
+        }
+        el.appendChild(childEl)
+    })
+    return el
+}
+
+// 更新DOM节点,要将变化的节点更新到真实DOM中，得传入根节点root，真实的节点和虚拟节点树形结构得相同
+function updateElement($root, newElem, oldElem, index = 0) {
+    if (!oldElem) {
+        // 如果没有旧节点，则渲染之前的节点，当做新的节点
+        $root.appendChild(newElem.render())
+    } else if (!newElem) {
+        // 如果之前的节点没有了，则删除
+        $root.removeChild($root.childNodes[index])
+    } else if (changed(newElem, oldElem)) {
+        if (typeof newElem === 'string') {
+            $root.childNodes[index].textContent = newElem
+        } else {
+            $root.replaceChild(newElem.render(), $root.childNodes[index])
+        }
+    } else if (newElem.tagName) {
+        for (let i = 0; i < newElem.children.length || i < oldElem.children.length; i++) {
+            updateElement($root.childNodes[index], newElem.children[i], oldElem.children[i], i)
+        }
+    }
+}
+
+function changed(newEle, oldEle) {
+    return (typeof newEle !== typeof oldEle) || (typeof newEle === 'string' && newEle !== oldEle)
+}
