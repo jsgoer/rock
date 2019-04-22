badef3d151b47ae7ba2694398ac47b226f1be2d2
igsnow <zzy716810918@gmail.com>
2019-04-22 19:35:14 +0800

    手写flat

---
+++ js/flat/handleFlat.js
@@ -0,0 +1,10 @@
+// 手写flat数组扁平化
+// 需要用到递归，所以不适合写到数组的原型上，直接定义函数
+
+function _flat(arr) {
+    return [].concat(...arr.map(item => Array.isArray(item) ? _flat(item) : item))
+}
+
+let arr = [[1, 2], 3, [[[4], 5]]]
+console.log(_flat(arr));
+
