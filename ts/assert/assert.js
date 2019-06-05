8e5f4b34bb2378454452c164d9e0dba68d82934c
igsnow <zzy716810918@gmail.com>
2019-06-05 16:18:17 +0800

    ts assert

---
+++ ts/assert/assert.js
@@ -0,0 +1,13 @@
+/**
+ *  当ts不能确定一个变量是哪个类型的时候，只能访问公共的属性和方法
+ *  此时可以使用断言，将something断言成string,否则会报错
+ *  断言不是类型转换,不可以断言成不存在的类型
+ */
+function getLength(something) {
+    if (something.length) {
+        return something.length;
+    }
+    else {
+        return something.toString().length;
+    }
+}
