89e7e393a08c16ea831f7c5bcc8fddc917027889
igsnow <zzy716810918@gmail.com>
2019-06-06 13:55:57 +0800

    ts 只读属性接口

---
+++ ts/interface/readonly.js
@@ -0,0 +1,7 @@
+var p3 = {
+    id: 1,
+    name: 'zzy',
+    age: 20,
+    sex: 'male'
+};
+console.log(p3);
---
+++ ts/interface/readonly.ts
@@ -0,0 +1,17 @@
+// 只读属性(要求对象中的属性只有在创建时被赋值)
+interface Person3 {
+    readonly id: number
+    name: string
+    age: number
+
+    [propName: string]: any
+}
+
+let p3: Person3 = {
+    id: 1,
+    name: 'zzy',
+    age: 20,
+    sex: 'male'
+}
+
+console.log(p3);
