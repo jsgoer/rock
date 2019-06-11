6e68909fa3543d9189b5250875b4b630bb319922
igsnow <zzy716810918@gmail.com>
2019-06-11 16:10:13 +0800

    ts 枚举

---
+++ ts/enum/enum.js
@@ -0,0 +1,12 @@
+// 如果枚举未被赋值，则会被赋值为从0开始递增的数字，同时枚举值到枚举名进行反向映射
+var Days;
+(function (Days) {
+    Days[Days["Sun"] = 3] = "Sun";
+    Days[Days["Mon"] = 1] = "Mon";
+    Days[Days["Tue"] = 2] = "Tue";
+    Days[Days["Wed"] = 3] = "Wed";
+    Days[Days["Thu"] = 4] = "Thu";
+    Days[Days["Fri"] = 5] = "Fri";
+    Days[Days["Sat"] = 6] = "Sat";
+})(Days || (Days = {}));
+console.log(Days);
---
+++ ts/enum/enum.ts
@@ -0,0 +1,12 @@
+// 如果枚举未被赋值，则会被赋值为从0开始递增的数字，同时枚举值到枚举名进行反向映射
+enum Days {
+    Sun = 3,
+    Mon = 1,
+    Tue,
+    Wed,
+    Thu,
+    Fri,
+    Sat
+}
+
+console.log(Days);
