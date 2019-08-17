000b5aba7c5b1d0fdc19e25b7abd74ba957b9e7b
igsnow <zzy716810918@gmail.com>
2019-08-17 14:15:29 +0800

    çç判断是否是ä普通对象

---
+++ utils/object/isPlainObject.js
@@ -0,0 +1,18 @@
+/**
+ * @param obj
+ * @desc 判断对象是否是普通对象(from redux)
+ * @return boolean
+ */
+function isPlainObject(obj) {
+    if (typeof obj !== 'object' || obj === null) return false
+    let proto = obj
+    while (Object.getPrototypeOf(proto) !== null) {
+        proto = Object.getPrototypeOf(proto)
+    }
+    return Object.getPrototypeOf(obj) === proto
+}
+
+let obj = {}     // true
+// let obj = Object.create(null)       // false
+console.log(isPlainObject(obj));
+
