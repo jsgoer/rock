ca949246e76a79284af60d912d78efd6a3f37c05
igsnow <zzy716810918@gmail.com>
2019-10-28 14:40:17 +0800

    测试更大的数据

--- js/test/main.js
+++ js/test/main.js
@@ -1,8 +1,11 @@
 let startTime = new Date().getTime();
+console.log('startTime : ' + startTime);
 let sum = 0;
 for (let i = 0; i < 987654321; i++) {
     sum = sum + i;
 }
 let entTime = new Date().getTime();
+console.log('entTime : ' + entTime);
+
 console.log(sum);
 console.log((entTime - startTime) / 1000);
