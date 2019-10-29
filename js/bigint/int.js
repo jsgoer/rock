d0137b2674600f1033e36e0f8bf99b75630bf8c2
igsnow <zzy716810918@gmail.com>
2019-10-29 15:43:29 +0800

    bigint

---
+++ js/bigint/int.js
@@ -0,0 +1,12 @@
+let n = BigInt(987654321);   // 构造函数
+console.log(n);
+
+let m = 987654321n;           // 结尾加n
+console.log(m);
+
+
+// 1.bigint不能传入Math等函数     2.不能喝number类型等相加
+// Math.max(m, 1)
+
+// let sum = m + 10
+// console.log(sum);
