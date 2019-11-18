9737c2eed4149bab9963a43ccf2d19b3ee066280
igsnow <zzy716810918@gmail.com>
2019-11-18 09:49:58 +0800

    array fill

--- js/array/arrayFrom.js
+++ js/array/arrayFrom.js
@@ -1,3 +1,8 @@
+/**
+ * 伪数组的定义
+ * 1.有可遍历的索引
+ * 2.有length属性
+ */
 // 将一个类数组或者可迭代对象创建一个新的、浅拷贝的数组
 console.log(Array.from('zhang'));
 
@@ -14,3 +19,7 @@ let targetObj = {
 console.log(Array.from([1, 2, 3, 4, 5], function (item) {
     return this.handle(item)
 }, targetObj));
+
+// {length:5}也是一个伪数组
+let arr = Array.from({length: 5}).fill(1, 1, 3)
+console.log(arr);
---
+++ js/array/fill.js
@@ -0,0 +1,3 @@
+// 填充数组
+let arr = Array(5).fill('z', 0, 3);
+console.log(arr);
