00e2b7c58eefcfc106d5cbb5c41d9736e620512d
igsnow <zzy716810918@gmail.com>
2019-11-18 09:38:27 +0800

    for async log

--- js/let&const/question.js
+++ js/let&const/question.js
@@ -1,9 +1,10 @@
-for (let i = 0; i < 3; i++) {
+// 通过改变变量作用域
+for (let i = 1; i <= 5; i++) {
     setTimeout(function () {
         console.log(i);
-    }, 0)
+    }, i * 1000);      // 间隔1s输出，相对于开始放入队列的时间间隔是一样的
 }
 
 
 console.log(a);   // undefined     var会出现变量提升，let不会
-var a = 1;
+var a = 5;
