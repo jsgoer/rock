2694c7ad36636976b35ff486575303dc674da8b4
igsnow <zzy716810918@gmail.com>
2019-11-30 20:17:00 +0800

    for in 和for of 区别回顾

--- js/for/in&&of.js
+++ js/for/in&&of.js
@@ -1,23 +1,29 @@
+// for in 和for of之间的区别
+
 let obj = {
-  name: 'zzy',
-  age: 18
+    name: 'zzy',
+    age: 18
 }
 
-let arr = [1, 2, 3]
+let arr = ['a', 'b', 'c']
 
 for (let i in arr) {
-  console.log(i)
+    console.log('i in array: ', i)
 }
+
+console.log('--------------------------')
 
 for (let i of arr) {
-  console.log(i)
+    console.log('i of array: ', i)
 }
+
+console.log('--------------------------')
 
 for (let i in obj) {
-  console.log(i)
+    console.log('i in obj: ', i)
 }
 
-// for of 不能用于json对象
+// 注：for of 不能用于json对象,不可迭代
 // for (let i of obj) {
-//   console.log(i)
+//     console.log('i of obj', i)
 // }
