89c57f30805ce09e60ae7775327794000057a9eb
igsnow <zzy716810918@gmail.com>
2020-12-31 10:12:27 +0800

    整理stream

--- js/stream/index.js
+++ js/stream/index.js
@@ -1,13 +1,11 @@
 const fs = require('fs')
 const path = require('path')
 
-const fileName1 = path.resolve(__dirname, 'test.pdf')
-const filename2 = path.resolve(__dirname, 'test2.pdf')
+const fileName1 = path.resolve(__dirname, 'test.txt')
+const filename2 = path.resolve(__dirname, 'test2.txt')
 
 // 读取流
 const readStream = fs.createReadStream(fileName1)
-
-console.log('readStream ', readStream);
 
 // 写入流
 const writeStream = fs.createWriteStream(filename2)
@@ -15,12 +13,35 @@ const writeStream = fs.createWriteStream(filename2)
 // 通过管道pipe写入
 readStream.pipe(writeStream)
 
+let chunks = [];
+
 // 通过监听data，是一个异步读取，回调函数打印每次读取的内容
 readStream.on('data', (chunk) => {
-    // console.log(chunk.toString());
+    chunks.push(chunk);
 })
 
 // 监听读取结束
 readStream.on('end', () => {
     console.log('copy done');
+    let result = Buffer.concat(chunks);
+    console.log('result:', result.toString('base64'), result.length);
+
+    fs.writeFile(path.resolve(__dirname, 'base64.txt'), result.toString('base64'), { 'flag': 'a' }, function (err) {
+        if (err) {
+            throw err;
+        }
+        console.log('ok.');
+    });
+
+
 })
+
+// 将base64编码转为stream生成目标pdf
+let base64str = ''
+
+let buff = new Buffer.from(base64str, 'base64');
+let stream = fs.createWriteStream(path.resolve(__dirname, 'base64.pdf'));
+stream.write(buff);
+stream.on("end", function () {
+    stream.end();
+}); 
